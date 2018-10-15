2e23061cfac448a268c66435f360ed64e7e0f3c2
Ray <tn710617@gmail.com>
2018-10-15 20:56:11 +0800

    Register function completed

---
+++ app/Http/Controllers/registrationController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class registrationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request)
+    {
+        $this->validate(request(),[
+            'name' => 'required|max:255',
+            'phone_number' => 'required|regex:/^\(?[+]?\d{1,3}\)?[-\s]?\d{3}[-\s]?\d{3}[-\s]?\d{3}$/',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|max:255'
+             ]);
+        $pattenForPhoneNumber = '/[\s\(\)\-\+]/';
+        $new_phone_number = preg_replace($pattenForPhoneNumber, '', $request->phone_number);
+
+        $user = User::forceCreate([
+            'name' => $request->name,
+            'email' =>  $request->email,
+            'phone_number' => $new_phone_number,
+            'password' => bcrypt($request->password)
+        ]);
+
+        Auth::login($user);
+        return ["result" => "true", "response" => "You've successfully registered"];
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,10 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('admin')->default('no');
             $table->rememberToken();
             $table->timestamps();
         });
